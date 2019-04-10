936afff326ab7d1d7b581679fbaae0fd3f0b9050
yangdianxp <yangdianxp@163.com>
2019-04-10 10:16:51 +0800

    ...

--- boostStudy/hana/test.cpp
+++ boostStudy/hana/test.cpp
@@ -1,3 +1,128 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+
+template<class ...Args>
+void printer(Args&&... args) {
+	(std::cout << ... << args) << '\n';
+	//(I op ... op E)
+	//((((I op E1) op E2) op ...) op EN)
+}
+
+template<class T, class... Args>
+void push_back_vec(std::vector<T>& v, Args&&... args)
+{
+	static_assert((std::is_constructible_v<T, Args&> && ...));
+	(v.push_back(args), ...);
+	//
+	//
+}
+
+// 基于 http://stackoverflow.com/a/36937049 的编译时端序交换 
+template<class T, std::size_t... N>
+constexpr T bswap_impl(T i, std::index_sequence<N...>) {
+	return (((i >> N * CHAR_BIT & std::uint8_t(-1)) << (sizeof(T) - 1 - N)*CHAR_BIT) | ...);
+}
+template<class T, class U = std::make_unsigned_t<T>>
+constexpr U bswap(T i) {
+	return bswap_impl<U>(i, std::make_index_sequence<sizeof(T)>{});
+}
+
+int main()
+{
+	printer(1, 2, 3, "abc");
+
+	std::vector<int> v;
+	push_back_vec(v, 6, 2, 45, 12);
+	push_back_vec(v, 1, 2, 9);
+	for (int i : v) std::cout << i << ' ';
+
+	static_assert(bswap<std::uint16_t>(0x1234u) == 0x3412u);
+	static_assert(bswap<std::uint64_t>(0x0123456789abcdefULL) == 0xefcdab8967452301ULL);
+
+	system("pause");
+	return 0;
+}
+
+
+#if 0
+#include <iostream>
+
+template<typename... Args>
+bool all(Args... args) { return (... && args); }
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
+
+
+bool b = all(true, true, true, false);
+// 在 all() 中，一元左折叠展开成
+//  return ((true && true) && true) && false;
+// b 为 false
+std::cout << b << std::endl;
+
+
+#include <boost/hana.hpp>
+#include <boost/any.hpp>
+#include <cassert>
+#include <string>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+namespace hana = boost::hana;
+//! [cases]
+template <typename T>
+auto case_ = [](auto f) {
+	return hana::make_pair(hana::type_c<T>, f);
+};
+struct default_t;
+auto default_ = case_<default_t>;
+//! [cases]
+//! [process]
+template <typename Any, typename Default>
+auto process(Any&, std::type_index const&, Default& default_) {
+	return default_();
+}
+template <typename Any, typename Default, typename Case, typename ...Rest>
+auto process(Any& a, std::type_index const& t, Default& default_,
+	Case& case_, Rest& ...rest)
+{
+	using T = typename decltype(+hana::first(case_))::type;
+	return t == typeid(T) ? hana::second(case_)(*boost::unsafe_any_cast<T>(&a))
+		: process(a, t, default_, rest...);
+}
+//! [process]
+//! [switch_]
+template <typename Any>
+auto switch_(Any& a) {
+	return [&a](auto ...cases_) {
+		auto cases = hana::make_tuple(cases_...);
+		auto default_ = hana::find_if(cases, [](auto const& c) {
+			return hana::first(c) == hana::type_c<default_t>;
+		});
+		static_assert(default_ != hana::nothing,
+			"switch is missing a default_ case");
+		auto rest = hana::filter(cases, [](auto const& c) {
+			return hana::first(c) != hana::type_c<default_t>;
+		});
+		return hana::unpack(rest, [&](auto& ...rest) {
+			return process(a, a.type(), hana::second(*default_), rest...);
+		});
+	};
+}
+//! [switch_]
+
+
+
+
+
 #include <cassert>
 #include <iostream>
 #include <string>
@@ -26,7 +151,7 @@ int main()
 }
 
 
-#if 0
+
 auto animals = hana::make_tuple(Fish{ "Nemo" }, Cat{ "Garfield" }, Dog{ "Snoopy" });
 // Access tuple elements with operator[] instead of std::get.
 Cat garfield = animals[1_c];
---
+++ other_code/input_output_manipulator.cpp
@@ -0,0 +1,51 @@
+﻿#include <sstream>
+#include <locale>
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	{
+		std::cout << "////////////////////////////////////////////////////////////////////////////////" << std::endl;
+
+		// boolalpha 输出
+		std::cout << std::boolalpha
+			<< "boolalpha true: " << true << '\n'
+			<< "boolalpha false: " << false << '\n';
+		std::cout << std::noboolalpha
+			<< "noboolalpha true: " << true << '\n'
+			<< "noboolalpha false: " << false << '\n';
+		// boolalpha 分析
+		bool b1, b2;
+		std::istringstream is("true false");
+		is >> std::boolalpha >> b1 >> b2;
+		std::cout << '\"' << is.str() << "\" parsed as " << b1 << ' ' << b2 << '\n';
+
+		std::cout << "////////////////////////////////////////////////////////////////////////////////" << std::endl;
+	}
+	
+	{
+		std::cout << "////////////////////////////////////////////////////////////////////////////////" << std::endl;
+
+		// showbase 影响八进制和十六进制的输出
+		std::cout << std::hex
+			<< "showbase: " << std::showbase << 42 << '\n'
+			<< "noshowbase: " << std::noshowbase << 42 << '\n';
+
+		// 及货币值的输入和输出
+		std::locale::global(std::locale("en_US.utf8"));
+		long double val = 0;
+		std::istringstream is("3.14");
+		is >> std::showbase >> std::get_money(val);
+		std::cout << "With showbase, parsing 3.14 as money gives " << val << '\n';
+		is.seekg(0);
+		is >> std::noshowbase >> std::get_money(val);
+		std::cout << "Without showbase, parsing 3.14 as money gives " << val << '\n';
+
+		std::cout << "////////////////////////////////////////////////////////////////////////////////" << std::endl;
+	}
+	
+
+	system("pause");
+	return 0;
+}
