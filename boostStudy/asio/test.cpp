ad2c9b15ac11115bf4cf14cc1e0a3e8a188538c7
yangdianxp <yangdianxp@163.com>
2019-04-19 10:29:13 +0800

    ...

--- boostStudy/asio/test.cpp
+++ boostStudy/asio/test.cpp
@@ -1,23 +1,162 @@
-#include <boost/asio/yield.hpp>
-#include <boost/asio/coroutine.hpp>
+//
+// echo_server.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/write.hpp>
 #include <iostream>
+#include <memory>
 
-boost::asio::coroutine c;
+using boost::asio::ip::tcp;
 
-void foo(int i)
+class session : public std::enable_shared_from_this<session>
 {
-	reenter(c)
+public:
+	explicit session(boost::asio::io_context& io_context, tcp::socket socket)
+		: socket_(std::move(socket)),
+		timer_(io_context),
+		strand_(io_context.get_executor())
 	{
-		yield std::cout << "foo1 " << i << std::endl;
-		fork foo(100);
-		yield std::cout << "foo2 " << i + 1 << std::endl;
 	}
-}
-int main()
+
+	void go()
+	{
+		auto self(shared_from_this());
+		boost::asio::spawn(strand_,
+			[this, self](boost::asio::yield_context yield)
+		{
+			try
+			{
+				char data[128];
+				for (;;)
+				{
+					timer_.expires_from_now(std::chrono::seconds(10));
+					std::size_t n = socket_.async_read_some(boost::asio::buffer(data), yield);
+					boost::asio::async_write(socket_, boost::asio::buffer(data, n), yield);
+				}
+			}
+			catch (std::exception& e)
+			{
+				socket_.close();
+				timer_.cancel();
+			}
+		});
+
+		boost::asio::spawn(strand_,
+			[this, self](boost::asio::yield_context yield)
+		{
+			while (socket_.is_open())
+			{
+				boost::system::error_code ignored_ec;
+				timer_.async_wait(yield[ignored_ec]);
+				if (timer_.expires_from_now() <= std::chrono::seconds(0))
+					socket_.close();
+			}
+		});
+	}
+
+private:
+	tcp::socket socket_;
+	boost::asio::steady_timer timer_;
+	boost::asio::strand<boost::asio::io_context::executor_type> strand_;
+};
+
+int main(int argc, char* argv[])
 {
-	foo(1);
-	foo(2);
-	foo(3);
-	system("pause");
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: echo_server <port>\n";
+			return 1;
+		}
+
+		boost::asio::io_context io_context;
+
+		auto func = [&](boost::asio::yield_context yield)
+		{
+			std::cout << "test test" << std::endl;
+		};
+
+		boost::asio::spawn(io_context, 
+			[&](boost::asio::yield_context yield)
+		{
+			std::cout << "test test" << std::endl;
+		});
+
+		//boost::asio::post(io_context, [&]()
+		//{
+		//	//std::cout << ec.message() << std::endl;
+		//	std::cout << "wa wa" << std::endl;
+		//});
+
+		io_context.post([&]()
+		{
+			//std::cout << ec.message() << std::endl;
+			std::cout << "wa wa" << std::endl;
+		});
+
+		boost::asio::spawn(io_context,
+			[&](boost::asio::yield_context yield)
+		{
+			tcp::acceptor acceptor(io_context,
+				tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
+
+			for (;;)
+			{
+				boost::system::error_code ec;
+				tcp::socket socket(io_context);
+				acceptor.async_accept(socket, yield[ec]);
+				if (!ec)
+				{
+					std::make_shared<session>(io_context, std::move(socket))->go();
+				}
+			}
+		});
+
+		io_context.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
 	return 0;
 }
+
+
+
+
+
+//#include <boost/asio/yield.hpp>
+//#include <boost/asio/coroutine.hpp>
+//#include <iostream>
+//
+//boost::asio::coroutine c;
+//
+//void foo(int i)
+//{
+//	reenter(c)
+//	{
+//		yield std::cout << "foo1 " << i << std::endl;
+//		fork foo(100);
+//		yield std::cout << "foo2 " << i + 1 << std::endl;
+//	}
+//}
+//int main()
+//{
+//	foo(1);
+//	foo(2);
+//	foo(3);
+//	system("pause");
+//	return 0;
+//}
