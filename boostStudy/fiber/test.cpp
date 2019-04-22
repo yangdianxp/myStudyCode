ad9df95e8d8e95431fa719b6648bea3737c49702
yangdianxp <yangdianxp@163.com>
2019-04-22 18:56:08 +0800

    ...

--- boostStudy/fiber/test.cpp
+++ boostStudy/fiber/test.cpp
@@ -5,6 +5,14 @@
 using namespace boost::fibers;
 using namespace boost::this_fiber;
 
+boost::fibers::fiber f1;
+
+void f() {
+	boost::fibers::fiber f2(some_fn);
+
+	f1 = std::move(f2); // f2 moved to f1
+}
+
 int main()
 {
 
