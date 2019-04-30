424adc479e2890407ac0893550f289d4ecbb93d2
yangdianxp <yangdianxp@163.com>
2019-04-30 11:08:15 +0800

    Update c++11_new_features.cpp

--- other_code/c++11_new_features.cpp
+++ other_code/c++11_new_features.cpp
@@ -6,6 +6,75 @@
 
 using namespace std;
 
+template <typename T> void TempFun(T a) {
+	cout << a << endl;
+}
+
+int main()
+{
+	TempFun(1);
+	TempFun("1");
+
+	system("pause");
+	return 0;
+}
+
+
+
+#if 0
+
+template <typename T> class DefenderT {
+public:
+	friend T;
+	void Defence(int x, int y) {}
+	void Tackle(int x, int y) {}
+private:
+	int pos_x = 15;
+	int pos_y = 0;
+	int speed = 2;
+	int stamina = 120;
+};
+
+template <typename T> class AttackerT {
+public:
+	friend T;
+	void Move(int x, int y) {}
+	void SpeedUp(float ratio) {}
+private:
+	int pos_x = 0;
+	int pos_y = -30;
+	int speed = 3;
+	int stamina = 100;
+};
+
+using Defender = DefenderT<int>;
+using Attacker = AttackerT<int>;
+
+#define UNIT_TEST
+#ifdef UNIT_TEST
+
+class Validator;
+using DefenderTest = DefenderT<Validator>;
+using AttackerTest = AttackerT<Validator>;
+
+class Validator {
+public:
+	void Validate(int x, int y, DefenderTest & d) {}
+	void Validate(int x, int y, AttackerTest & a) {}
+};
+
+DefenderTest d;
+AttackerTest a;
+a.Move(15, 30);
+d.Defence(15, 30);
+a.SpeedUp(1.5f);
+d.Defence(15, 30);
+Validator v;
+v.Validate(7, 0, d);
+v.Validate(1, -10, a);
+
+#endif
+
 class Mem {
 public:
 	Mem(int i) : m(i) {}
@@ -26,15 +95,6 @@ private:
 	Mem mem{ 0 };
 	string name{ "Group" };
 };
-
-int main()
-{
-	
-
-	system("pause");
-}
-
-#if 0
 
 struct Mem {
 	Mem() { cout << "Mem default, num: " << num << endl; }
