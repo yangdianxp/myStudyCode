0d0f5c24dd6fed683b00c1670db37cc5ec651387
yangdianxp <yangdianxp@163.com>
2019-05-23 14:25:09 +0800

    。。。

---
+++ other_code/effective_modern_c++_study.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template<typename T>
+void f(T& param)
+{
+
+}
+
+int main()
+{
+	int x = 27;
+	const int cx = x;
+	const int& rx = x;
+
+	f(x);
+	f(cx);
+	f(rx);
+	system("pause");
+}
+
+
+
+#if 0
+17
+
+
+#endif
