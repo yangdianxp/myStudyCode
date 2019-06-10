b1dd0ef60bfcdac450789a1eef3d0d116e35b39d
yangdianxp <yangdianxp@163.com>
2019-06-10 21:53:46 +0800

    ...

---
+++ other_code/the_c++_standard_library_study.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main()
+{
+	system("pause");
+	return 0;
+}
+
+#if 0
+
+	int array[] = { 1, 2, 3, 4, 5 };
+	long sum = 0;
+	for (int x : array) {
+		sum += x;
+	}
+	for (auto elem : { sum, sum * 2, sum * 4 }) {
+		std::cout << elem << std::endl;
+	}
+
+
+#endif
